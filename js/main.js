378713e6e98e9670d5680c9db4c65f280310a538
Mitchell Peterson <mitchellnevinpeterson@gmail.com>
2018-03-22 12:54:48 -0700

    added sound effects and some finishing touches on apperance

--- js/main.js
+++ js/main.js
@@ -7,49 +7,62 @@ new Vue({
 		counter: 0
 	},
 	methods: {
+		monsterTimedAttack: function() {
+			if(this.playerHealth > 35){
+			var redHealth = document.getElementById("player-health")
+			redHealth.classList.toggle("red")
+			}
+		},
 		minusHealth: function() {
 			if(this.isRunning == false) {
 				return
 			}
 			this.playerHealth -= 1
-			if(this.playerHealth > 35){
-			var redHealth = document.getElementById("player-health")
-			redHealth.classList.toggle("red")
-			}
 			if(this.playerHealth < 35){
 				document.getElementById("player-health").className = "low-health"
+			}
+			if(this.monsterHealth < 50){
+				document.getElementById("monster-health").className = "low-health"
 			}
 			this.checkWin()
 		},
 		timedHealth: function() {
-			setInterval(this.minusHealth, 1500)
+			setInterval(this.minusHealth, 2000)
+			setInterval(this.monsterTimedAttack, 600)
 			this.checkWin()
 		},
 		start: function() {
+			document.getElementById("startSound").play()
 			this.counter = 0
 			this.isRunning = true
 			this.timedHealth()
 		},
 		giveUp: function() {
-			location.reload()
+			document.getElementById("giveUpSound").play()
+			setTimeout(function() {
+				location.reload()
+			}, 1100)
 			// this.isRunning = false
 			// this.playerHealth = 100
 			// this.monsterHealth = 100
 
 		},
 		attack: function() {
+			document.getElementById("attackSound").play()
 			this.playerHealth -= Math.floor(Math.random() * 14) + 6
 			this.monsterHealth -= Math.floor(Math.random() * 10) + 3
 			this.checkWin()
 			this.counter += 1
 		},
 		special:function() {
+			document.getElementById("specialSound").play()
 			this.playerHealth -= Math.floor(Math.random() * 18) + 6
 			this.monsterHealth -= Math.floor(Math.random() * 13) + 5
 			this.counter += 1
 			this.checkWin()
 		},
 		heal: function() {
+			document.getElementById("healSound").play()
 			if(this.counter < 1) {
 				return
 			}
@@ -78,12 +91,18 @@ new Vue({
 		},
 		newGame: function(outcome) {
 				if(confirm("You " + outcome + "! New game?")) {
-					location.reload()
+					document.getElementById("giveUpSound").play()
+					setTimeout(function() {
+						location.reload()
+					}, 1050)
 					// this.playerHealth = 100
 					// this.monsterHealth = 100
 					// this.counter = 0
 				} else {
-					location.reload()
+					document.getElementById("giveUpSound").play()
+					setTimeout(function() {
+						location.reload()
+					}, 1050)
 					// this.counter = 0
 					// this.start()
 				}
